6229d850b7a16d49a199e77ca360e02b57fbe441
Kaiser <dev.khagonia@gmail.com>
2023-02-11 13:08:53 +0800

    File restructure
    Restructured files so that the project can be deployed on Render.

--- test/js/index.js
+++
@@ -1,8 +0,0 @@
-const FORM_LOGIN = document.querySelector('.login-form')
-
-FORM_LOGIN.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  const formData = new FormData(FORM_LOGIN);
-  console.log(formData);
-})
